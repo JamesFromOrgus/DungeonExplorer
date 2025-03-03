ed8131ba8edeebdd5eb4ec66ecbaa370cdda32c6
james <jamesharrisonwalker@outlook.com>
2025-03-03 15:51:05 +0000

    Added typewriting effect along with ability to skip.

---
+++ Display.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Threading;
+
+namespace DungeonExplorer
+{
+    public static class Display
+    {
+        private static int rate = 20;
+        private static string currentMessage;
+        private static int charsWritten;
+
+        private static void WriteChars(int amount)
+        {
+            string substring = currentMessage.Substring(charsWritten, amount);
+            Console.Write(substring);
+            charsWritten += amount;
+        }
+
+        public static void Write(string message, bool newLine = true)
+        {
+            if (newLine)
+            {
+                message += "\n";
+            }
+            currentMessage = message;
+            charsWritten = 0;
+            for (int i = 0; i < message.Length; i++)
+            {
+                if (Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Enter)
+                {
+                    WriteChars(message.Length - charsWritten);
+                    break;
+                }
+                WriteChars(1);
+                Thread.Sleep(1000/rate);
+            }
+        }
+    }
+}
--- Game.cs
+++ Game.cs
@@ -12,31 +12,34 @@ namespace DungeonExplorer
         public Game()
         {
             // Initialize the game with one room and one player
-
+            
         }
         public void Start()
         {
-            Menu choice = new Menu("Pick a word:", new[]
-            {
-                new Choice("Option A", () =>
-                {
-                    Console.WriteLine("ok you picked option A");
-                    Console.WriteLine("Multiline function lol");
-                }, ConsoleColor.Red),
-                new Choice( "Option B", () => Console.WriteLine("ok you picked option B"))
-            });
-            choice.Open();
+            // Menu choice = new Menu("Pick a word:", new[]
+            // {
+            //     new Choice("Option A", () =>
+            //     {
+            //         Console.WriteLine("ok you picked option A");
+            //         Console.WriteLine("Multiline function lol");
+            //     }, ConsoleColor.Red),
+            //     new Choice( "Option B", () => Console.WriteLine("ok you picked option B"))
+            // });
+            // choice.Open();
+            //
+            // DialogueNode ping = new DialogueNode("The guy", "Ping");
+            // DialogueNode nextNode = new DialogueNode("The guy", "I'm glad you wanted to hear more! 'ping' is an expression relating to the popular sport, Table Tennis.\n" +
+            //     "It is commonly referred to as 'Ping-Pong', hence why 'pong' was provided as an option.\nUs going back and forth with 'ping' and 'pong' is reminiscent of\n" +
+            //     "a ping-pong ball oscillating from one side of the table to another.");
+            // ping.AddResponse("Pong", ping);
+            // ping.AddResponse("Please elaborate.", nextNode);
+            // ping.AddChoice("Leave");
+            // nextNode.AddResponse("Interesting, let's continue.", ping);
+            //
+            // ping.Display();
 
-            DialogueNode ping = new DialogueNode("The guy", "Ping");
-            DialogueNode nextNode = new DialogueNode("The guy", "I'm glad you wanted to hear more! 'ping' is an expression relating to the popular sport, Table Tennis.\n" +
-                "It is commonly referred to as 'Ping-Pong', hence why 'pong' was provided as an option.\nUs going back and forth with 'ping' and 'pong' is reminiscent of\n" +
-                "a ping-pong ball oscillating from one side of the table to another.");
-            ping.AddResponse("Pong", ping);
-            ping.AddResponse("Please elaborate.", nextNode);
-            ping.AddChoice("Leave");
-            nextNode.AddResponse("Interesting, let's continue.", ping);
-
-            ping.Display();
+            Display.Write("Here is a test. I am gonna fill this up with words so it takes long enough to write to the" +
+                          "\nthe screen as I would like to test some things such as skipping the typewriting effect.");
 
             // Change the playing logic into true and populate the while loop
             bool playing = false;
